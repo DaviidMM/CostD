2b3f287f29173a65751d1898a71c8d68858319d1
DaviidMM <davicitoo1612@gmail.com>
2022-08-28 21:23:57 +0200

    Add expense creation feature

--- components/ExpensesPanel/index.js
+++ components/ExpensesPanel/index.js
@@ -1,9 +1,22 @@
 import { useState } from 'react';
 import Button from '../Button';
 import Expense from '../Expense';
+import NewExpenseForm from '../NewExpenseForm';
 
-export default function ExpensesPanel({ expenses, members = [] }) {
+export default function ExpensesPanel(expense) {
+  const { expenses: initialExpenses, members } = expense;
+
+  const [expenses, setExpenses] = useState(initialExpenses);
   const [active, setActive] = useState(null);
+  const [showNewExpenseForm, setShowNewExpenseForm] = useState(false);
+
+  const addExpense = (expense) => {
+    setExpenses([...expenses, expense]);
+  };
+
+  const removeExpense = (expense) => {
+    setExpenses(expenses.filter((e) => e.id !== expense.id));
+  };
 
   const toggleExpense = (expense) => {
     if (active === expense) return setActive(null);
@@ -12,20 +25,37 @@ export default function ExpensesPanel({ expenses, members = [] }) {
 
   return (
     <div>
-      <ul className="flex flex-col gap-2 mb-4">
-        {expenses.map((expense) => {
-          return (
-            <Expense
-              key={expense.id}
-              {...expense}
-              open={expense.id === active}
-              toggleExpense={toggleExpense}
-              members={members}
-            />
-          );
-        })}
-      </ul>
-      <Button color="orange">💵 Añadir gasto</Button>
+      {showNewExpenseForm ? (
+        <NewExpenseForm
+          onCreate={addExpense}
+          closeForm={() => setShowNewExpenseForm(false)}
+          members={members}
+        />
+      ) : (
+        <>
+          <ul className="flex flex-col gap-2 mb-4">
+            {expenses.map((expense) => {
+              return (
+                <Expense
+                  key={expense.id}
+                  {...expense}
+                  members={members}
+                  onDelete={removeExpense}
+                  open={expense.id === active}
+                  toggleExpense={toggleExpense}
+                />
+              );
+            })}
+          </ul>
+          <Button
+            className="mt-4"
+            color="orange"
+            onClick={() => setShowNewExpenseForm(true)}
+          >
+            💵 Añadir gasto
+          </Button>
+        </>
+      )}
     </div>
   );
 }
