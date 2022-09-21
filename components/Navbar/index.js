2de93fd44fba58d3f15b7b5db426cd61bfc7fb69
DaviidMM <73941850+DaviidMM@users.noreply.github.com>
2022-09-21 10:19:47 +0200

    Close menu on any click in mobiles

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,10 +1,11 @@
-export default function Navbar({ children, className, showMenu }) {
+export default function Navbar({ children, showMenu, onClick }) {
   return (
     <nav
       className={
         (showMenu ? 'block ' : 'hidden ') +
-        'absolute top-0 bottom-0 left-0 right-0 z-40 py-20 bg-white md:block md:static md:bg-inherit md:backdrop-blur-0 bg-opacity-40 backdrop-blur-sm md:py-0'
+        'absolute inset-0 z-40 py-20 bg-white md:block md:static md:bg-inherit md:backdrop-blur-0 bg-opacity-40 backdrop-blur-sm md:py-0'
       }
+      onClick={onClick}
     >
       <ul className="flex flex-col items-center gap-4 mx-auto font-bold text-black md:text-white md:flex-row w-fit">
         {children}
